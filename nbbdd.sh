d4ebe184d60f8789109449af247527a562764a58
Patrick McCann <pgm5@st-andrews.ac.uk>
2020-04-02 15:58:05 +0100

    Complete process

--- nbbdd.sh
+++ nbbdd.sh
@@ -5,4 +5,14 @@ for nb in *.ipynb; do
     for feature in *.feature; do
         python generate_steps.py $feature
     done
+    index=1
+    for dir in $nb*/; do
+        for testfile in "$dir/*"; do
+            cat ${testfile} >> test$index
+        done
+        ((index++))
+    done
+    python insert_tests.py test* $nb
+    mv $nb $nb-old
+    mv $nb.new $nb
 done
