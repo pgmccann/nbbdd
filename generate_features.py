f98bc3130dfe56880c695cb6b2b401c9005dcf53
Patrick McCann <pgm5@st-andrews.ac.uk>
2020-04-02 10:48:09 +0100

    Parses gherkin blocks

--- generate_features.py
+++ generate_features.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python
 
+"""
+Parses Gherkin code from a Jupyter Notebook.
+
+Takes the path to a Jupyter Notebook as an argument
+
+Gherkin code should be marked up in Markdown cells in blocks of the form
+
+```gherkin
+Feature: ...
+    Scenario: ...
+        ...
+```
+"""
+
 import sys
 import json
 import os.path
@@ -16,9 +30,21 @@ if not os.path.isfile(nb_file_path):
     raise ValueError('Specified file does not exist')
 
 with open(nb_file_path) as nb_file:
+
     data = json.load(nb_file)
     cells = data["cells"]
-    for cell in cells:
-        cell_type = cell["cell_type"]
-        if cell_type == "markdown":
-            print(cell["source"])
+
+    for i, cell in enumerate(cells):
+        if cell["cell_type"] == "markdown":
+            source = cell["source"]
+            gherkin_code = ""
+            in_gherkin_block = False
+            for line in source:
+                if line.startswith("```gherkin"):
+                    in_gherkin_block = True
+                elif in_gherkin_block:
+                    if line.startswith("```"):
+                        in_gherkin_block = False
+                    else:
+                        gherkin_code += line
+            print(gherkin_code)
